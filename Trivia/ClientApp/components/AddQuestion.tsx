ab0d97f7b6f8184510c932199a9aebf45927d0e3
Johanna <32160638+johannaastrom@users.noreply.github.com>
2018-05-08 10:44:57 +0200

    added component files

---
+++ Trivia/ClientApp/components/AddQuestion.tsx
@@ -0,0 +1,19 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import 'isomorphic-fetch';
+import { NavMenu } from './NavMenu';
+
+interface IAddQuestionProps { }
+interface IAddQuestionState { }
+
+export class AddQuestion extends React.Component<RouteComponentProps<{}>, {}> {
+	public render() {
+		return <div>
+			<p>You can add a new trivia question here!</p>
+			<input type="text" placeholder="Add new question" /> <br />
+			<input type="text" placeholder="Add option" /> <br />
+			<input type="text" placeholder="Add option" /> <br />
+			<input type="text" placeholder="Add option" /> <br />
+		</div>;
+	}
+}
