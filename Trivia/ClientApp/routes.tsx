3f7f93f933bb3cf0ea22c049c79777282dacf839
Johanna <32160638+johannaastrom@users.noreply.github.com>
2018-05-08 13:18:22 +0200

    added routes

--- Trivia/ClientApp/routes.tsx
+++ Trivia/ClientApp/routes.tsx
@@ -4,9 +4,13 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { Highscore } from './components/Highscore';
+import { AddQuestion } from './components/AddQuestion';
 
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
     <Route path='/counter' component={ Counter } />
-    <Route path='/fetchdata' component={ FetchData } />
+	<Route path='/fetchdata' component={FetchData} />
+	<Route path='/highscore' component={Highscore} />
+	<Route path='/question' component={AddQuestion} />
 </Layout>;
