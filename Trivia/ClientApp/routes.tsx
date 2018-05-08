de89d0a03e6ea4fd11d12443658b79d63cb44aed
Johanna <32160638+johannaastrom@users.noreply.github.com>
2018-05-08 15:10:38 +0200

    changed navigation

--- Trivia/ClientApp/routes.tsx
+++ Trivia/ClientApp/routes.tsx
@@ -12,5 +12,5 @@ export const routes = <Layout>
     <Route path='/counter' component={ Counter } />
 	<Route path='/fetchdata' component={FetchData} />
 	<Route path='/highscore' component={Highscore} />
-	<Route path='/question' component={AddQuestion} />
+	<Route path='/addQuestion' component={AddQuestion} />
 </Layout>;
