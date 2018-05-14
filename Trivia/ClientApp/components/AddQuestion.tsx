39dd8bd8889ff915782a6b836d526546b4201bac
Andekl <andreas.eklund94@gmail.com>
2018-05-14 23:38:19 +0200

    added forms and fetch to AddQuestion and new route to QuestionsController

--- Trivia/ClientApp/components/AddQuestion.tsx
+++ Trivia/ClientApp/components/AddQuestion.tsx
@@ -6,9 +6,10 @@ import { NavMenu } from './NavMenu';
 interface IAddQuestionProps { }
 interface IAddQuestionState {
     questionName: string;
-    correctAnswer: string;
-    falseOption1: string;
-    falseOption2: string;
+    correctOption: string;
+    OptionA: string;
+    OptionB: string;
+    OptionC: string;
 }
 
 export class AddQuestion extends React.Component<RouteComponentProps<IAddQuestionProps>, IAddQuestionState> {
@@ -16,12 +17,17 @@ export class AddQuestion extends React.Component<RouteComponentProps<IAddQuestio
         super();
         this.state = {
             questionName: "",
-            correctAnswer: "",
-            falseOption1: "",
-            falseOption2: ""
+            correctOption: "",
+            OptionA: "",
+            OptionB: "",
+            OptionC: ""
         };
 
         this.handleChangeNewQuestion = this.handleChangeNewQuestion.bind(this);
+        this.handleChangeOptionA = this.handleChangeOptionA.bind(this);
+        this.handleChangeOptionB = this.handleChangeOptionB.bind(this);
+        this.handleChangeOptionC = this.handleChangeOptionC.bind(this);
+        this.handleChangeCorrectOption = this.handleChangeCorrectOption.bind(this);
         this.handleClick = this.handleClick.bind(this);
 
         fetch('api/Questions') // post question method??
@@ -33,27 +39,74 @@ export class AddQuestion extends React.Component<RouteComponentProps<IAddQuestio
     }
     public render() {
         return <div>
-            <div className="addQuestion">
-                <p>You can add a new trivia question here!</p>
-                <input type="text" placeholder="Add new question"
-                    value={this.state.questionName}
-                    onChange={this.handleChangeNewQuestion} /> <br />
+            <h1>You can add a new trivia question here!</h1>
+            <div className="form-group">
+                <div className="addQuestion">
+
+                    <label htmlFor="text">Question: </label>
+                    <input type="text" placeholder="Add new question"
+                        value={this.state.questionName}
+                        onChange={this.handleChangeNewQuestion} /> <br />
+                </div>
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="OptionA">Option A: </label>
+                <input type="text" placeholder="Add option" className="form-control" id="OptionA"
+                    value={this.state.OptionA}
+                    onChange={this.handleChangeOptionA} /> <br />
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="OptionB">Option B: </label>
+                <input type="text" placeholder="Add option" className="form-control" id="OptionB"
+                    value={this.state.OptionB}
+                    onChange={this.handleChangeOptionB} /> <br />
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="OptionC">Option C: </label>
+                <input type="text" placeholder="Add option" className="form-control" id="OptionC"
+                    value={this.state.OptionC}
+                    onChange={this.handleChangeOptionC} /> <br />
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="CorrectOption"> Correct Option: </label>
+                <input type="text" className="form-control" id="CorrectOption"
+                    value={this.state.correctOption}
+                    onChange={this.handleChangeCorrectOption}
+                    placeholder="A, B or C" />
             </div>
 
 
-
-            <input type="text" placeholder="Add option" /> <br />
-            <input type="text" placeholder="Add option" /> <br />
-            <input type="text" placeholder="Add option" /> <br />
-            <button onClick={this.handleClick}> Submit question! </button>
+            <button className="btn btn-default" onClick={this.handleClick}> Submit question! </button>
         </div>;
     }
 
     handleChangeNewQuestion(event: any) {
         this.setState({ questionName: event.target.value });
     }
+    handleChangeOptionA(event: any) {
+        this.setState({ OptionA: event.target.value });
+    }
+    handleChangeOptionB(event: any) {
+        this.setState({ OptionB: event.target.value });
+    }
+    handleChangeOptionC(event: any) {
+        this.setState({ OptionC: event.target.value });
+    }
+    handleChangeCorrectOption(event: any) {
+        this.setState({ correctOption: event.target.value });
+    }
+
+
+
     handleClick(event: any) {
-        fetch('api/AddQuestion?question=' + this.state.questionName)
-        .then//updatera databas, lägg till ny rad.
+        fetch('api/Questions/NewQuestion?questionName=' + this.state.questionName + '&optionA=' + this.state.OptionA + '&optionB=' + this.state.OptionB + '&optionC=' + this.state.OptionC + '&correctOption=' + this.state.correctOption)
+            .then(Response => {
+                console.log(Response);
+                console.log("******console after response*****");
+            })
     }
 }
--- Trivia/Migrations/20180514134759_change.Designer.cs
+++ Trivia/Migrations/20180514134759_change.Designer.cs
@@ -3,10 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.EntityFrameworkCore.Storage;
-using Microsoft.EntityFrameworkCore.Storage.Internal;
-using React.Trivia.Data;
 using System;
+using Trivia.Data;
 
 namespace Trivia.Migrations
 {
